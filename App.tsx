4a3f79d53a370ac330b9fa771821cf061cbe7714
Suane Vallim <suane.vallim@ilegra.com>
2023-01-28 10:24:45 -0300

    refat: onesignal install, managed -> bare workflow

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import OneSignal from "react-native-onesignal";
+
+OneSignal.setAppId("98c9e6e0-cf1a-473c-aecd-25fde8ae3ef9");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
